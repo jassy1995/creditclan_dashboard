9b44c6f0525225038fc0348e232f1f91cab86225
jassy1995 <babatundejoseph85@gmail.com>
2022-02-03 18:22:35 +0100

    added the approve method

--- models/comment-house.model.js
+++ models/comment-house.model.js
@@ -1,28 +1,24 @@
 const CommentHouse = (sequelize, DataTypes, Sequelize) =>
-  sequelize.define(
-    "comment_houses",
-    {
-      comment_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        allowNull: false,
-        autoIncrement: true,
-      },
-      comment: {
-        type: DataTypes.TEXT,
-      },
-      user_id: {
-        type: DataTypes.TEXT,
-      },
-      date_added: {
-        type: DataTypes.DATEONLY,
-      },
-
-      house_id: {
-        type: DataTypes.TEXT,
-      },
+  sequelize.define("comment_houses", {
+    comment_house_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
     },
-    { timestamp: false }
-  );
+    comment: {
+      type: DataTypes.TEXT,
+    },
+    user_id: {
+      type: DataTypes.TEXT,
+    },
+    date_added: {
+      type: DataTypes.DATEONLY,
+    },
+
+    house_id: {
+      type: DataTypes.TEXT,
+    },
+  });
 
 module.exports = CommentHouse;
--- models/comment-request.model.js
+++ models/comment-request.model.js
@@ -1,28 +1,24 @@
 const CommentRequest = (sequelize, DataTypes, Sequelize) =>
-  sequelize.define(
-    "comment_request",
-    {
-      comment_request_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        allowNull: false,
-        autoIncrement: true,
-      },
-      comment: {
-        type: DataTypes.TEXT,
-      },
-      user_id: {
-        type: DataTypes.TEXT,
-      },
-      date_added: {
-        type: DataTypes.DATEONLY,
-      },
-
-      request_id: {
-        type: DataTypes.TEXT,
-      },
+  sequelize.define("comment_request", {
+    comment_request_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
     },
-    { timestamp: false }
-  );
+    comment: {
+      type: DataTypes.TEXT,
+    },
+    user_id: {
+      type: DataTypes.TEXT,
+    },
+    date_added: {
+      type: DataTypes.DATEONLY,
+    },
+
+    request_id: {
+      type: DataTypes.TEXT,
+    },
+  });
 
 module.exports = CommentRequest;
