10c3fae3fc644a81209fdc2d601519bec6f0cc1c
jassy1995 <babatundejoseph85@gmail.com>
2022-02-17 16:23:44 +0100

    added comment

--- models/restaurant.model.js
+++ models/restaurant.model.js
@@ -1,48 +1,52 @@
 const Restaurant = (sequelize, DataTypes, Sequelize) =>
-  sequelize.define("restaurant", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      allowNull: false,
-      autoIncrement: true,
+  sequelize.define(
+    "restaurant",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      location: {
+        type: DataTypes.TEXT,
+      },
+      lat: {
+        type: DataTypes.TEXT,
+      },
+      lng: {
+        type: DataTypes.TEXT,
+      },
+      account_number: {
+        type: DataTypes.TEXT,
+      },
+      bank_code: {
+        type: DataTypes.TEXT,
+      },
+      meal_amounts: {
+        type: DataTypes.TEXT,
+      },
+      waiters: {
+        type: DataTypes.TEXT,
+      },
+      phone: {
+        type: DataTypes.TEXT,
+      },
+      restaurant_name: {
+        type: DataTypes.TEXT,
+      },
+      payload: {
+        type: DataTypes.TEXT,
+      },
+      image: {
+        type: DataTypes.TEXT,
+      },
+      is_approved: {
+        type: DataTypes.STRING,
+      },
     },
-    location: {
-      type: DataTypes.TEXT,
-    },
-    lat: {
-      type: DataTypes.TEXT,
-    },
-    lng: {
-      type: DataTypes.TEXT,
-    },
-    account_number: {
-      type: DataTypes.TEXT,
-    },
-    bank_code: {
-      type: DataTypes.TEXT,
-    },
-    meal_amounts: {
-      type: DataTypes.TEXT,
-    },
-    waiters: {
-      type: DataTypes.TEXT,
-    },
-    phone: {
-      type: DataTypes.TEXT,
-    },
-    restaurant_name: {
-      type: DataTypes.TEXT,
-    },
-    payload: {
-      type: DataTypes.TEXT,
-    },
-    image: {
-      type: DataTypes.TEXT,
-    },
-    is_approved: {
-      type: DataTypes.STRING,
-    },
-  });
+    { timestamps: false }
+  );
 
 module.exports = Restaurant;
 
