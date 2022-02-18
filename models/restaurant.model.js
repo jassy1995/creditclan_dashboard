5c7cadd4bc1034379ec6511c9137c2ed8a88acae
jassy1995 <babatundejoseph85@gmail.com>
2022-02-18 15:21:57 +0100

    added comment

--- models/restaurant.model.js
+++ models/restaurant.model.js
@@ -1,57 +1,61 @@
 const Restaurant = (sequelize, DataTypes, Sequelize) =>
-  sequelize.define("restaurant", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      allowNull: false,
-      autoIncrement: true,
+  sequelize.define(
+    "restaurant",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      location: {
+        type: DataTypes.TEXT,
+      },
+      lat: {
+        type: DataTypes.TEXT,
+      },
+      lng: {
+        type: DataTypes.TEXT,
+      },
+      account_number: {
+        type: DataTypes.TEXT,
+      },
+      bank_code: {
+        type: DataTypes.TEXT,
+      },
+      meal_amounts: {
+        type: DataTypes.TEXT,
+      },
+      waiters: {
+        type: DataTypes.TEXT,
+      },
+      phone: {
+        type: DataTypes.TEXT,
+      },
+      restaurant_name: {
+        type: DataTypes.TEXT,
+      },
+      payload: {
+        type: DataTypes.TEXT,
+      },
+      image: {
+        type: DataTypes.TEXT,
+      },
+      is_approved: {
+        type: DataTypes.INTEGER,
+      },
+      step: {
+        type: DataTypes.INTEGER,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+      },
     },
-    location: {
-      type: DataTypes.TEXT,
-    },
-    lat: {
-      type: DataTypes.TEXT,
-    },
-    lng: {
-      type: DataTypes.TEXT,
-    },
-    account_number: {
-      type: DataTypes.TEXT,
-    },
-    bank_code: {
-      type: DataTypes.TEXT,
-    },
-    meal_amounts: {
-      type: DataTypes.TEXT,
-    },
-    waiters: {
-      type: DataTypes.TEXT,
-    },
-    phone: {
-      type: DataTypes.TEXT,
-    },
-    restaurant_name: {
-      type: DataTypes.TEXT,
-    },
-    payload: {
-      type: DataTypes.TEXT,
-    },
-    image: {
-      type: DataTypes.TEXT,
-    },
-    is_approved: {
-      type: DataTypes.INTEGER,
-    },
-    step: {
-      type: DataTypes.INTEGER,
-    },
-    created_at: {
-      type: DataTypes.DATE,
-    },
-    updated_at: {
-      type: DataTypes.DATE,
-    },
-  });
+    { timestamps: false }
+  );
 
 module.exports = Restaurant;
 
