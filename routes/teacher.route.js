8076feb9342c7e24c9b67b1d8f6e08cfb1246f56
jassy1995 <babatundejoseph85@gmail.com>
2022-02-22 13:25:15 +0100

    solving db issue

--- routes/teacher.route.js
+++ routes/teacher.route.js
@@ -12,6 +12,6 @@ router.post("/api/comment-teacher-loan", teacherLoanComment);
 router.post("/api/get-teacher-comment", getTeacherComment);
 router.post("/api/pre-approve-teacher", preApprovalWorkFlowTeacher);
 router.post("/api/get-all-teacher-loan", getAllTeachersLoan);
-router.post("/api/get-summary-workflow-teacher", getSummaryOfRequestStage);
+router.get("/api/get-summary-workflow-teacher", getSummaryOfRequestStage);
 
 module.exports = router;
