feb53a87bb0b5c4ce4765daf335a99d2b23eae85
jassy1995 <babatundejoseph85@gmail.com>
2022-02-22 08:28:50 +0100

    solving db issue

--- models/teachers.model.js
+++ models/teachers.model.js
@@ -1,74 +1,78 @@
 const Teacher = (sequelize, DataTypes, Sequelize) =>
-  sequelize.define("teacher", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      allowNull: false,
-      autoIncrement: true,
+  sequelize.define(
+    "teacher",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      school_id: {
+        type: DataTypes.TEXT,
+      },
+      name: {
+        type: DataTypes.TEXT,
+      },
+      email: {
+        type: DataTypes.TEXT,
+      },
+      phone: {
+        type: DataTypes.TEXT,
+      },
+      salary: {
+        type: DataTypes.TEXT,
+      },
+      loan_request_link: {
+        type: DataTypes.TEXT,
+      },
+      bank: {
+        type: DataTypes.TEXT,
+      },
+      bank_number: {
+        type: DataTypes.TEXT,
+      },
+      account_name: {
+        type: DataTypes.TEXT,
+      },
+      loan_eligibility: {
+        type: DataTypes.TEXT,
+      },
+      deleted: {
+        type: DataTypes.TEXT,
+      },
+      bvn: {
+        type: DataTypes.TEXT,
+      },
+      picture: {
+        type: DataTypes.TEXT,
+      },
+      salary_payment: {
+        type: DataTypes.STRING,
+      },
+      payment_method: {
+        type: DataTypes.STRING,
+      },
+      date_of_birth: {
+        type: DataTypes.STRING,
+      },
+      gender: {
+        type: DataTypes.STRING,
+      },
+      is_approved: {
+        type: DataTypes.INTEGER,
+      },
+      step: {
+        type: DataTypes.INTEGER,
+      },
+      created_at: {
+        type: DataTypes.STRING,
+      },
+      updated_at: {
+        type: DataTypes.STRING,
+      },
     },
-    school_id: {
-      type: DataTypes.TEXT,
-    },
-    name: {
-      type: DataTypes.TEXT,
-    },
-    email: {
-      type: DataTypes.TEXT,
-    },
-    phone: {
-      type: DataTypes.TEXT,
-    },
-    salary: {
-      type: DataTypes.TEXT,
-    },
-    loan_request_link: {
-      type: DataTypes.TEXT,
-    },
-    bank: {
-      type: DataTypes.TEXT,
-    },
-    bank_number: {
-      type: DataTypes.TEXT,
-    },
-    account_name: {
-      type: DataTypes.TEXT,
-    },
-    loan_eligibility: {
-      type: DataTypes.TEXT,
-    },
-    deleted: {
-      type: DataTypes.TEXT,
-    },
-    bvn: {
-      type: DataTypes.TEXT,
-    },
-    picture: {
-      type: DataTypes.TEXT,
-    },
-    salary_payment: {
-      type: DataTypes.STRING,
-    },
-    payment_method: {
-      type: DataTypes.STRING,
-    },
-    date_of_birth: {
-      type: DataTypes.STRING,
-    },
-    gender: {
-      type: DataTypes.STRING,
-    },
-    is_approved: {
-      type: DataTypes.INTEGER,
-    },
-    step: {
-      type: DataTypes.INTEGER,
-    },
-    created_at: {
-      type: DataTypes.STRING,
-    },
-    updated_at: {
-      type: DataTypes.STRING,
-    },
-  });
+    { timestamps: false }
+  );
 
 module.exports = Teacher;
