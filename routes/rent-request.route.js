dbf2d0c64781b7ad62057bf34ccbaf4d06c3b9a7
jassy1995 <babatundejoseph85@gmail.com>
2022-02-23 07:44:02 +0100

    approve rent added

--- routes/rent-request.route.js
+++ routes/rent-request.route.js
@@ -2,10 +2,12 @@ const express = require("express");
 const {
   rentRequestComment,
   getAllRentComment,
+  preApprovalWorkFlowRent,
 } = require("../controllers/rent-request.controller");
 const router = express.Router();
 
 router.post("/api/comment-request", rentRequestComment);
 router.post("/api/get-rent-request-comment", getAllRentComment);
+router.post("/api/approve-rent", preApprovalWorkFlowRent);
 
 module.exports = router;
